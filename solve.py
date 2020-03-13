73791d2af7965856cf68a9b3e329b38431f67231
karasuex54 <karasuyama1997@gmail.com>
2020-03-13 16:11:46 +0900

    complete solve

--- solve.py
+++ solve.py
@@ -1,5 +1,56 @@
-def main():
-    print(1)
+# pulp v.1.6.0
 
+import json
+import pulp
+def read_json():
+    res = []
+    with open("./menus.json") as f:
+        df = json.load(f)
+    N = len(df)
+    menu_names = []
+    price = []
+    energy = []
+    protein = []
+    fat = []
+    carbohydrates = []
+    salt = []
+    red = []
+    green = []
+    yellow = []
+    d = ["menu_name","price","energy","protein","fat","carbohydrates","salt","red","green","yellow"]
+    for i in range(N):
+        s = str(i)
+        menu_names.append(df[s][d[0]])
+        price.append(df[s][d[1]])
+        energy.append(df[s][d[2]])
+        protein.append(df[s][d[3]])
+        fat.append(df[s][d[4]])
+        carbohydrates.append(df[s][d[5]])
+        salt.append(df[s][d[6]])
+        red.append(df[s][d[7]])
+        green.append(df[s][d[8]])
+        yellow.append(df[s][d[9]])
+    return N,[menu_names,price,energy,protein,fat,carbohydrates,salt,red,green,yellow]
+def main():
+    N,df = read_json()
+    
+    problem = pulp.LpProblem("Problem-2",pulp.LpMinimize)
+    x = [pulp.LpVariable("x{}".format(str(i)),cat="Binary") for i in range(N)]
+    print(x)
+    print(df[1])
+    problem+=pulp.lpDot(x,df[1])
+    problem+=pulp.lpDot(x,df[2])>=883 # energy
+    # protein
+    problem+=pulp.lpDot(x,df[4])<=25 # fat
+    # carbohydrates
+    problem+=pulp.lpDot(x,df[6])<=2 # salt
+    problem+=pulp.lpDot(x,df[7])>=2 # red
+    problem+=pulp.lpDot(x,df[8])>=1 # green
+    problem+=pulp.lpDot(x,df[9])>=5 # yellow
+    print(problem)
+    status=problem.solve()
+    if status==1:
+        print([df[0][i] for i in range(len(x)) if x[i].value() == 1])
+        print(problem.objective.value())
 if __name__=="__main__":
     main()
--- test.py
+++ test.py
@@ -1,10 +1,53 @@
-def check_code():
-    print("a :",ord("a"),"z :",ord("z"))
-    print("A :",ord("A"),"Z :",ord("Z"))
-    for i in range(10):print(i,ord(str(i)),end="")
-    print()
-    print("(",ord("("))
-def main():
-    check_code()
-if __name__=="__main__":
-    main()
+# coding: utf-8
+
+# Dummy Nihongo: あ（私の使っているテキストエディタにUTF-8と認識させるため）
+
+
+# Import
+# -----------------------------------------------------------------------------
+# MIPCL-PY
+import mipcl_py.mipshell.mipshell as mp
+# -----------------------------------------------------------------------------
+
+
+# Main
+# -----------------------------------------------------------------------------
+print(f'-' * 40)
+print(f'[宣言]')
+
+# 数理最適化問題を宣言（名前をつける必要あり）
+prob = mp.Problem('Problem-1')
+
+# 変数を宣言（連続変数）
+# 引数を全部指定する場合
+x = mp.Var(name='x', type=mp.REAL, lb=0.0, ub=mp.VAR_INF, priority=0)
+# 引数をけっこう省略する場合（type以降のデフォルト値は↑と同じ）
+y = mp.Var(name='y')
+
+# 目的関数を宣言（最大化）
+mp.maximize(x + y)
+
+# 制約条件を宣言
+2 * x + y <= 2
+x + 2 * y <= 2
+
+
+print(f'-' * 40)
+print(f'[計算]')
+
+# 計算（計算ログを出力）
+prob.optimize(silent=False)
+
+print(f'-' * 40)
+print(f'[出力]')
+# テキスト出力
+prob.printSolution()
+
+print(f'-' * 32)
+# 目的関数値や変数の値を個別に取得
+print(f'目的関数値 = {prob.getObjVal()}')
+print(f'変数 x の値 = {x.val}')
+print(f'変数 y の値 = {y.val}')
+
+print(f'-' * 40)
+# -----------------------------------------------------------------------------
